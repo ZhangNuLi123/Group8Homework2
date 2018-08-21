e7560033a59a2b0b31cf3ea80dc2503f68f39c4e
unknown <you@example.com>
2018-08-21 11:50:00 +0800

    add some explaination

--- TictactoeGame/Game 1.cpp
+++ TictactoeGame/Game 1.cpp
@@ -2,31 +2,41 @@
 #include <ctime>
 using namespace std;
 
-int CheckNumber(char [], int );
-void Change(char [], int , int );
-void print(char []);
-int CheckStatus(char []);
+int CheckNumber(char [], int );               //检查输入位置信息是否合法
+void Change(char [], int , int );             //下棋改变棋盘
+void Print(char []);                          //展示棋盘实现低级交互
+int CheckStatus(char []);                     //检验游戏是否结束
 
 int main()
 {   
-    srand((unsigned int)time(0));
-	int player=0;
-	int number=0;
-	char chess[10]="123456789";
-    cout<<"棋盘如下"<<endl;
+    srand((unsigned int)time(0));              //电脑下棋随机数代码
+	
+	int player=0;                              //记录下棋者
+	int number=0;                              //记录下棋位置
+	char chess[10]="123456789";                //初始化棋盘
+    int winner=0;     
+
+	cout<<"棋盘如下"<<endl;                           
 	print(chess);
-	int winner=0;
+	                          //状态参数
 	cout<<"请设置谁先手"<<endl;
 	cout<<"1--玩家执O		2--电脑执X"<<endl;
-	cin>>player;
-    int state=0;
-	for( ;winner==0;player++)
+	
+	cin>>player;                                       //设置先手
+	while（player!=1&&player!=2）                      //先手合法性判断
+	{
+		cout << "请输入合法玩家编号" << endl;
+	cin >> player;
+	}
+    
+	int state=0;                                //设置下棋个数，当棋盘棋子达到了9，且未结束时判断为平局
+	for(player;winner==0;player++)
 	{   
 		number=0;
-   	    if(player==3) 
+   	    if(player==3)                           //判断player是否溢出
 			player=player-2;
 
-        if(player==1)
+        if(player==1)                            
 		{   
 			cout<<"请您下棋"<<endl;
 		    cin>>number;
@@ -37,8 +47,8 @@ int main()
 		    cin>>number;
 			};
  
-		    Change(chess, number, player);
-			print(chess);
+		    Change(chess, number, player);    
+			Print(chess);
            
 			if(CheckStatus(chess)==1)		
 			{
@@ -57,8 +67,10 @@ int main()
 			{
 			number=(rand()%10);
 			};
+			
 			Change(chess, number, player);
-			print(chess);
+			Print(chess);
+			
 			if(CheckStatus(chess)==1)		
 			{
 				cout<<"电脑获得胜利"<<endl;   
@@ -66,7 +78,11 @@ int main()
 			};
 		}
 		state++;
-		if(state==9) {cout<<"平局"<<endl;winner=1;};
+		if(state==9) 
+		{
+			cout<<"平局"<<endl;
+			winner=1;
+		};
 
 	}
 
@@ -75,9 +91,7 @@ int main()
 }
 
 
-
-
-int CheckNumber(char chess[], int num)
+int CheckNumber(char chess[], int num)             //函数定义，数组传参
 {
 	if (num<1||num>9)
 		return 0;
@@ -87,15 +101,15 @@ int CheckNumber(char chess[], int num)
 		return 1;
 }
 
-void Change(char chess[], int num, int player)
+void Change(char chess[], int num, int player)     //函数定义，传输参数
 {   
 	if (player==1)		
-		chess[num-1]='O';
+		chess[num-1]='O';               //根据下棋者更改棋盘
 	else		        
-		chess[num-1]='X';
+		chess[num-1]='X';               //根据下棋者更改棋盘
 }
 
-void print(char chess[])
+void Print(char chess[])                 //展示棋盘
 {
 	int i=0,m=0;
 	for (i=0; i<9; i++)
@@ -107,7 +121,7 @@ void print(char chess[])
 	}
 }
 
-int CheckStatus(char chess[10])
+int CheckStatus(char chess[10])           //检查是否成功
 {
 	int Status;
 	if(chess[0]==chess[3]&&chess[3]==chess[6]||chess[1]==chess[4]&&chess[4]==chess[7]||chess[2]==chess[5]&&chess[5]==chess[8])
